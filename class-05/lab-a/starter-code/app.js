841b3f01dee575b026cf966da730596c4f5cee26
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-02-18 19:28:11 +0200

    solving sumArray() in the problem-4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -80,12 +80,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    let sumParamArray = 0;
+    for (let index = 0; index < sumArr.length; index++) {
+        sumParamArray = sum(sumParamArray, sumArr[index])[0];
+    }
+    let message = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumParamArray} is their sum.`;
 
+    return [sumParamArray, message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
